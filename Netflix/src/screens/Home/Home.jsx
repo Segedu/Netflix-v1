c3f9332513c5bfa425aa610b3d2ab37e8851bb05
Segedu <seggedu20@gmail.com>
2022-01-22 23:50:36 +0200

    search function with suggestion

--- Netflix/src/screens/Home/Home.jsx
+++ Netflix/src/screens/Home/Home.jsx
@@ -21,11 +21,16 @@ const Home = ({ data, watchList, setWatchList }) => {
             <button onClick={() => removeFromList(display.id, watchList, setWatchList, "watchList")}>remove from watch list</button>
         </article>)
 
+    const suggestionHandler = (searchTerm) => {
+        setSearchTerm(searchTerm);
+        setSuggestions([]);
+    }
+
     return (
         <div className="MainContainer">
             <input onChange={(e) => searchData(e.target.value, data, setSuggestions, setSearchTerm)} className="searchInput" type="search" inputMode="search" placeholder="Type movie/Tv series..." />
             <div>
-                {suggestions ? suggestions.map((suggestion, i) => <article key={i}><p>{suggestion.title}</p></article>) : ""}
+                {searchTerm ? suggestions.map((suggestion, i) => <article key={i}><p onClick={() => suggestionHandler(suggestion.title)}>{suggestion.title}</p></article>) : ""}
             </div>
             <div className="HomePageTrailer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, atque.
                 Placeat sed voluptatum qui asperiores tenetur fugiat deserunt cumque totam!
@@ -33,10 +38,7 @@ const Home = ({ data, watchList, setWatchList }) => {
                 Facere iure eum ad architecto aliquam in hic dolores eos.
                 Vitae accusantium dolorem quibusdam? Dolores dolorum nobis impedit est ratione!
                 Accusantium, optio repudiandae soluta iste vero aspernatur error deserunt in.
-                Unde dolorum, atque perspiciatis quasi illum deserunt natus eaque ea.
-                Earum dolorum incidunt cum tempore pariatur nemo facilis beatae minima.
-                Aut numquam officiis ipsam nam excepturi dignissimos molestias architecto fugiat.
-                Officiis amet laborum beatae vitae, ut neque sint veniam sunt.</div>
+            </div>
             {Elements}
         </div>)
 }
