32f5c24de98e1d157788acf052f449891ec78eba
Segedu <seggedu20@gmail.com>
2022-01-22 18:56:11 +0200

    search bar with trailer space

--- Netflix/src/screens/Home/Home.jsx
+++ Netflix/src/screens/Home/Home.jsx
@@ -4,7 +4,6 @@ import { addToList, removeFromList } from '../../utils/utils';
 const Home = ({ data, watchList, setWatchList }) => {
     const [state, setState] = useState("");
 
-
     const Elements = data.map(display =>
         <article key={display.id}>
             {/* <video width="750" height="500" autoPlay loop muted >
@@ -18,11 +17,21 @@ const Home = ({ data, watchList, setWatchList }) => {
             <p>{display.actors}</p>
             <button onClick={() => addToList(data, display.id, watchList, setWatchList, "watchList")}>add to watch list</button>
             <button onClick={() => removeFromList(display.id, watchList, setWatchList, "watchList")}>remove from watch list</button>
-
         </article>)
+
     return (
         <div className="MainContainer">
-            <h1>home page</h1>
+            <input className="searchInput" type="search" inputMode="search" placeholder="Type movie/Tv series..." />
+            <div className="HomePageTrailer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, atque.
+                Placeat sed voluptatum qui asperiores tenetur fugiat deserunt cumque totam!
+                Ipsum quasi expedita perspiciatis ullam porro quae atque. Laborum, ab?
+                Facere iure eum ad architecto aliquam in hic dolores eos.
+                Vitae accusantium dolorem quibusdam? Dolores dolorum nobis impedit est ratione!
+                Accusantium, optio repudiandae soluta iste vero aspernatur error deserunt in.
+                Unde dolorum, atque perspiciatis quasi illum deserunt natus eaque ea.
+                Earum dolorum incidunt cum tempore pariatur nemo facilis beatae minima.
+                Aut numquam officiis ipsam nam excepturi dignissimos molestias architecto fugiat.
+                Officiis amet laborum beatae vitae, ut neque sint veniam sunt.</div>
             {Elements}
         </div>)
 }
